1d21bc94268d2073891f924f94231f0de0f41553
mohamed zakarya mohamed farag <mohamed.zakrya.farag@gmail.com>
2024-10-05 12:15:49 -0700

    we

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""the task :0-pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """returning list of integers and representing pascal triangle of n and returning empty list """
+    k = []
+    if n <= 0:
+        return k
+    k = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(k[i - 1]) - 1):
+            curr = k[i - 1]
+            temp.append(k[i - 1][j] + k[i - 1][j + 1])
+        temp.append(1)
+        k.append(temp)
+    return k
